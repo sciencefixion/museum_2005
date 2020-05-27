69112de584828bfb78d8eadfb6c480eea43429de
Rion Hutsell <st.gish@gmail.com>
2020-05-27 09:45:29 -0600

    Add attributes test to MuseumTest

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -12,4 +12,10 @@ class MuseumTest < Minitest::Test
     assert_instance_of Museum, dmns
   end
 
+  def test_it_can_read_attributes
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_equal "Denver Museum of Nature and Science", dmns.name
+    assert_equal [], dmns.exhibits
+  end
 end
