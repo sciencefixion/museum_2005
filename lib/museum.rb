391ae20b3c269eacb9fbaaa1924a00332d807ace
Rion Hutsell <st.gish@gmail.com>
2020-05-27 10:43:51 -0600

    Add admit method to Museum

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,12 @@
 class Museum
   attr_reader :name,
-              :exhibits
+              :exhibits,
+              :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -21,4 +23,7 @@ class Museum
     recommended
   end
 
+  def admit(patron)
+    @patrons << patron
+  end
 end
