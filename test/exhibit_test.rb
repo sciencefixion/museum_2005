fb32953a1ceaf1a407d1dcbb1900b78c04ac28b3
Rion Hutsell <st.gish@gmail.com>
2020-05-27 09:12:11 -0600

    Add PatronTest

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -1,12 +1,11 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/exhibit'
-require './lib/patron'
 
 class ExhibitTest < Minitest::Test
 
   def test_it_exists
-    exhibit = Exhibit.new
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
     assert_instance_of Exhibit, exhibit
   end
