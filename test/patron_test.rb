0a05f5f8a912ef23add4d722010a77122e4d80fe
Rion Hutsell <st.gish@gmail.com>
2020-05-27 09:19:20 -0600

    Add tests to PatronTest

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -11,4 +11,21 @@ class PatronTest < Minitest::Test
     assert_instance_of Patron, patron_1
   end
 
+  def test_it_can_read_attributes
+    skip
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal "Bob", patron_1.name
+    assert_equal 20, patron_1.spending_money
+    assert_equal [], patron_1.interests
+  end
+
+  def test_it_can_add_interests
+    skip
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests
+  end
 end
